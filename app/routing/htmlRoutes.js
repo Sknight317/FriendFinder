3564613a1e3b96f128f837220df69cac2048e84a
Sknight317 <Sknight23@mail.valenciacollege.edu>
2018-11-11 20:13:54 -0500

    new files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,12 @@ var path = require("path");
 module.exports = function(app) {
     // HTML GET Requests
     app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../survey.html"));
+      res.sendFile(path.join(__dirname, "../public/survery.html"));
     });
   
   
     // If no matching route is found default to home
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../home.html"));
+      res.sendFile(path.join(__dirname, "../public/home.html"));
     });
   };
